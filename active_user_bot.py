7afab41127e678d4fb93388c43f05e7664e237ea
Norman Moeschter <norman.moeschter@gmail.com>
2019-09-20 18:24:30 +0200

    Adapt project.

--- active_user_bot.py
+++ active_user_bot.py
@@ -1,25 +1,134 @@
 #! /usr/bin/python3
 
+"""
+
+CLIENT_SECRET=$(pass show work/CSS/reddit/subreddit_user_count) CLIENT_ID=$(pass show work/CSS/reddit/subreddit_user_count_id) PASSWORD=$(pass show work/CSS/reddit/xmrto-community) USERNAME=$(pass show work/CSS/reddit/xmrto-community_user) python active_user_bot.py
+
+python active_user_bot.py --secret <client_secret> --id <client_id> --password <reddit_password> --user <reddit_username>
+
+"""
+
 # Imports
+import os
 import praw
 import time
-import csv
+import json
 import datetime as dt
-import matplotlib.pyplot as plt
+import logging
+import argparse
+
+from utils import database
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+DEBUG = False
+MIN_RUNS = 1
+MIN_INTERVAL = 1
+
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET", None)
+CLIENT_ID = os.environ.get("CLIENT_ID", None)
+USERNAME = os.environ.get("USERNAME", None)
+PASSWORD = os.environ.get("PASSWORD", None)
+RUNS = MIN_RUNS
+INTERVAL = MIN_INTERVAL
+
+# read securely stored environment variables set in AWS Lambda
+# Use different variables locally
+if "SERVERTYPE" in os.environ and os.environ["SERVERTYPE"] == "AWS Lambda":
+    import boto3
+    from base64 import b64decode
+    ENCRYPTED = os.environ["DATABASE_URL"]
+    # Decrypt code should run once and variables stored outside of the function
+    # handler so that these are decrypted once per container
+    DATABASE_URL = bytes.decode(
+        boto3.client("kms").decrypt(CiphertextBlob=b64decode(ENCRYPTED))[
+            "Plaintext"
+        ]
+    )
+    CLIENT_SECRET = bytes.decode(
+        boto3.client("kms").decrypt(CiphertextBlob=b64decode(CLIENT_SECRET))[
+            "Plaintext"
+        ]
+    )
+    CLIENT_ID = bytes.decode(
+        boto3.client("kms").decrypt(CiphertextBlob=b64decode(CLIENT_ID))[
+            "Plaintext"
+        ]
+    )
+    PASSWORD = bytes.decode(
+        boto3.client("kms").decrypt(CiphertextBlob=b64decode(PASSWORD))[
+            "Plaintext"
+        ]
+    )
+    USERNAME = bytes.decode(
+        boto3.client("kms").decrypt(CiphertextBlob=b64decode(USERNAME))[
+            "Plaintext"
+        ]
+    )
+    DB_TYPE = database.POSTGRES
+else:
+    parser = argparse.ArgumentParser(
+        description="Get number of subscribers and active user count of subreddit.", epilog="", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    group_auth = parser.add_argument_group('authentication')
+    group_auth.add_argument(
+        "-s", "--secret", help="Reddit app's client secret."
+    )
+    group_auth.add_argument(
+        "-i", "--id", help="Reddit app's client id."
+    )
+    group_auth.add_argument(
+        "-p", "--password", help="Reddit password."
+    )
+    group_auth.add_argument(
+        "-u", "--user", help="Reddit username."
+    )
+    group = parser.add_argument_group('behaviour')
+    
+    group.add_argument(
+        "--runs", default=MIN_RUNS, type=int, help="How often active users should be retreived. Used in combination with --interval. If set to <=1 it runs once."
+    )
+    group.add_argument(
+        "--interval", default=MIN_INTERVAL, type=int, help="Waiting period [seconds] between runs. Used in combination with --runs."
+    )
+    args = parser.parse_args()
+    
+    if not CLIENT_SECRET:
+        CLIENT_SECRET = args.secret
+    
+    if not CLIENT_ID:
+        CLIENT_ID = args.id
+    
+    if not PASSWORD:
+        PASSWORD = args.password
+    
+    if not USERNAME:
+        USERNAME = args.user
+    
+    RUNS = args.runs
+    INTERVAL = args.interval
+    log.setLevel(logging.DEBUG)
+    # DB_TYPE = database.POSTGRES
+    # DATABASE_URL = "data.db"
+    DB_TYPE = database.POSTGRES
+    DATABASE_URL = "postgres@localhost:5432/test"
 
 class OnlineUserBot:
     """A simple bot that finds the number of users online a specifc Subreddit."""
 
-    def __init__(self, subreddit_name, interval, lengthOfTime, client_id, client_secret, password, user_agent, username):
+    def __init__(self, subreddit_name, interval, runs, client_id, client_secret, password, user_agent, username, debug=False):
         self.subreddit_name = subreddit_name
-        self.interval = interval
-        self.lengthOfTime = lengthOfTime
-        self.client_id = client_id
+        self.interval = max(int(interval), MIN_INTERVAL)
+        self.runs = max(int(runs), MIN_RUNS)
         self.client_id = client_id
         self.client_secret = client_secret
         self.password = password
         self.user_agent = user_agent
         self.username = username
+        self.db = None
+        self.debug = debug
 
     def auth(self):
         self.reddit = praw.Reddit(client_id = self.client_id,
@@ -30,81 +139,51 @@ class OnlineUserBot:
 
         self.subreddit = self.reddit.subreddit(self.subreddit_name)
 
-    def findNumberOnlineUsers(self):
+    def get_users_online(self):
         return self.subreddit.accounts_active
 
-    def write_information_to_CSV(self, _online_users, _readable_time):
-        with open('data.csv', "a") as csvFile:
-            filewriter = csv.writer(csvFile)
-            filewriter.writerow([_online_users, _readable_time])
-            csvFile.close()
+    def get_subscribers(self):
+        return self.subreddit.subscribers
 
-    def activateBot(self):
+    def get_public_description(self):
+        return self.subreddit.public_description
 
-        elapsed_time = time.clock()
+    def save_to_db(self, subreddit):
+        if self.db and not self.debug:
+            self.db.insert_(database.SUBREDDITS, subreddit)
 
-        while elapsed_time < self.lengthOfTime:
+    def __check_users(self):
+        self.auth()
+        time_checked = dt.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
+        users_online = self.get_users_online()
+        subscribers = self.get_subscribers()
 
-            self.auth()
+        subreddit = {"subreddit": self.subreddit_name, "subscribers": subscribers, "users_online": users_online, "created": time_checked}
 
-            readable_time = str(dt.datetime.now().time().strftime("%H-%M-%S"))
-            elapsed_time = time.clock()
-            online_users = self.findNumberOnlineUsers()
+        self.save_to_db(subreddit=subreddit)
 
-            self.write_information_to_CSV(online_users, readable_time)
+        log.info(json.dumps(subreddit))
 
-            print(elapsed_time)
-            time.sleep(self.interval)
-
-
-# Plot data
-def plotData(subreddit_name):
-    # create list holders for our data.
-    number_of_online_users = []
-    dateAndTime= []
-
-    with open('data.csv', "r") as csv_file:
-
-        csv_reader = csv.reader(csv_file, delimiter = ',')
-
-        for row in csv_reader:
-            try:
-                number_of_online_users.append(int(row[0]))
-                time = row[1]
-                dateAndTime.append(time)
-            except Exception as e:
-                if e == IndexError:
-                    pass
+    def run_bot(self):
+        if self.runs <= MIN_RUNS:
+            self.__check_users()
+        elif self.runs > MIN_RUNS:
+            for i in range(self.runs):
+                self.__check_users()
+                time.sleep(self.interval)
 
 
-    # Print data
-    print(number_of_online_users)
-    print(dateAndTime)
+def check_online_users(event, context):
+    subreddits = (
+        "Monero",
+    )
+    for subreddit in subreddits:
+        # Change the input to this class with your information from the Reddit API (See README.md for instructions)
+        bot = OnlineUserBot(subreddit_name=subreddit, interval=INTERVAL, runs=RUNS, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, password=PASSWORD, user_agent='subreddit_user_count1.0', username=USERNAME, debug=DEBUG)
+        bot.db = database.Db(dbtype=DB_TYPE, dbname=DATABASE_URL)
+        bot.run_bot()
 
-    plt.plot(dateAndTime, number_of_online_users)
-    plt.xlabel('Time')
-    plt.ylabel('Online Users')
-    plt.title('Subreddit: ' + subreddit_name)
-
-    ax = plt.subplot()
-
-    every_nth = 40
-    for i, tick in enumerate(ax.xaxis.get_ticklabels()):
-        if i % every_nth != 0:
-            tick.set_visible(False)
-
-    for i, line in enumerate(ax.xaxis.get_ticklines()):
-            if i % every_nth != 0:
-                line.set_visible(False)
-
-    plt.xticks(fontsize=8, rotation=60)
-
-    plt.show()
 
 if __name__ == '__main__':
+    check_online_users(event=None, context=None)
 
-    # Change the input to this class with your information from the Reddit API (See README.md for instructions)
-    bot = OnlineUserBot('SUBREDDIT_NAME', INTERVAL, TOTAL_TIME, 'CLIENT_ID','CLIENT_SECRET','PASSWORD','USER_AGENT','USERNAME')
-
-    bot.activateBot()
-    plotData('SUBREDDIT_NAME')
